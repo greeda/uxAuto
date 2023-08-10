4b286828b72f9aa9582dc198f63338b72af7ecbc
greeda <greeda@gmail.com>
2023-08-10 18:31:55 +0900

    0810

--- dist/src/js/components/UISwiper.js
+++ dist/src/js/components/UISwiper.js
@@ -2,11 +2,30 @@ import UI from './base/UI';
 
 class UISwiper extends UI {
   defaultOptions = {
-    
+
   };
 
   constructor(el, options) {
     super('UISwiper', el);
+
+    this.render();
+  }
+
+  render() {
+    this.detachEvents();
+    this.attachEvents();
+  }
+
+  attachEvents() {
+    
+  }
+
+  detachEvents() {
+
+  }
+
+  destroy() {
+
   }
 }
 
